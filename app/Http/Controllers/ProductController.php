6bb14a56da5ed6da5be83bf90dcdebd1b05c2cd5
DmitryFaber <mistertwister698@gmail.com>
2023-05-03 18:35:38 +0700

    update ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,21 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
-use App\Models\Type;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
     //
     public function index() {
-
-        $types = DB::table('product_types')
-            ->join('images', 'product_types.image_id', '=', 'images.id')
-            ->select('product_types.*', 'images.path_to_directory')
-            ->where('type_relationship_id', null)
-            ->get();
+        $product = new Category();
+        $types = $product->getCategories();
         return view('products.index', compact('types'));
     }
 
@@ -24,22 +19,11 @@ class ProductController extends Controller
             substr($request['category'], strripos($request['category'], '/') +1):
         $request['category'];
 
+        $product = new Category();
 
-        $types = DB::table('type_relationships')
-            ->join('product_types', 'product_types.type_relationship_id', '=', 'type_relationships.id')
-            ->join('images', 'product_types.image_id', '=', 'images.id')
-            ->select('product_types.*', 'images.path_to_directory')
-            ->where('type_relationships.product_type_id', $category)
-            ->get();
+        $types = $product->getSubcategories($category);
+        $products= $product->getProducts($category);
 
-        $products = DB::table('products')
-            ->join('images', 'products.image_id', '=', 'images.id')
-            ->select('products.id', 'products.product_name', 'images.path_to_directory')
-            ->where('products.product_type_id', $category)
-            ->get();
-
-       $dc = compact('types');
-       //dd($dc);
         return view('products.index', compact('types', 'products'));
 
 
@@ -48,13 +32,8 @@ class ProductController extends Controller
 
     public function item(Request $request) {
         $item = $request['item'];
-        $item_info = DB::table('products')
-            ->join('product_details', 'products.id','=', 'product_details.product_id')
-            ->join('images', 'products.image_id', '=', 'images.id')
-            ->select('products.*', 'product_details.*', 'images.path_to_directory')
-            ->where('products.id', $item)
-            ->get();
-        $item_info = $item_info[0];
+        $product = new Product();
+        $item_info = $product->getProduct($item);
         return view('products.item', compact('item_info'));
 
 
